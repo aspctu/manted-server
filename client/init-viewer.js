f673762fd613913c5c0a24a99519f84e1cae1415
Yunyu Lin <mail@yunyul.in>
2020-04-11 19:21:52 -0600

    Initial works

--- client/init-viewer.js
+++ client/init-viewer.js
@@ -7,8 +7,23 @@ export const initViewer = (roomName) => {
     ws.send(JSON.stringify({ type: "joinRoom", roomName }));
   });
 
+  let player = null;
+  let queuedFrames = [];
+
   ws.addEventListener("message", (msg) => {
     const { type, ...message } = JSON.parse(msg.data);
-    console.log(type, message);
+    if (type === "REPLAY_BATCH") {
+      const replayFrames = message.frames;
+      const lastReplayTimestamp =
+        replayFrames[replayFrames.length - 1][0].timestamp;
+      for (const frame of queuedFrames) {
+        if (frame[0].timestamp > lastReplayTimestamp) {
+          replayFrames.push(frame);
+        }
+      }
+      queuedFrames = [];
+      player = new rrweb.Replayer(replayFrames.map(([frame]) => frame));
+      player.play();
+    }
   });
 };
