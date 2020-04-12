3e8eb554c9650626e153cdb3cc3b60bba691c766
Yunyu Lin <mail@yunyul.in>
2020-04-12 07:57:14 -0600

    Add compression

--- index.js
+++ index.js
@@ -6,7 +6,24 @@ const app = express();
 app.use(express.static("client/public"));
 const server = http.createServer(app);
 
-const wss = new WebSocket.Server({ server });
+const wss = new WebSocket.Server({
+  server,
+  perMessageDeflate: {
+    zlibDeflateOptions: {
+      chunkSize: 1024,
+      memLevel: 7,
+      level: 3,
+    },
+    zlibInflateOptions: {
+      chunkSize: 10 * 1024,
+    },
+    clientNoContextTakeover: true,
+    serverNoContextTakeover: true,
+    serverMaxWindowBits: 10,
+    concurrencyLimit: 10,
+    threshold: 1024,
+  },
+});
 
 /*
 rooms: Map<string, {host: WSCLient, guests: WSClient[], replayFrames: any[] }>
