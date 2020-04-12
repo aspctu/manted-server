f524a1c41b17956fe8aa5c9fef6fed1bdf19bed3
Yunyu Lin <mail@yunyul.in>
2020-04-11 18:22:28 -0600

    Room logic

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: 8082 });
 
 /*
 rooms: Map<string, {host: WSCLient, guests: WSClient[]}>
@@ -9,21 +9,36 @@ rooms: Map<string, {host: WSCLient, guests: WSClient[]}>
 const rooms = new Map();
 
 wss.on("connection", (ws) => {
+  // let connectedRoom = null;
+
   ws.on("message", (msg) => {
-    const message = JSON.parse(msg);
-    if (message.type === "makeRoom") {
+    const { type, ...message } = JSON.parse(msg);
+    if (type === "makeRoom") {
       const { roomName } = message;
+      console.log("room receive", roomName);
       if (!rooms.has(roomName)) {
         console.log(`Making room ${roomName}`);
         rooms.set(roomName, { host: ws, guests: [] });
       }
-    } else if (message.type === "joinRoom") {
+    } else if (type === "joinRoom") {
       const { roomName } = message;
       if (rooms.has(roomName)) {
         rooms.get(roomName).guests.push(ws);
+        connectedRoom = roomName;
       }
-    } else if (message.type === "sendFrame") {
-    } else if (message.type === "sendDelta") {
+    } else if (type === "frames") {
+      console.log(message);
     }
   });
+
+  /*
+  ws.on("close", (ws) => {
+    const room = rooms.get(connectedRoom) || [];
+    const idx = room.indexOf(idx);
+    if (idx > -1) {
+      room.splice(idx, 1);
+    }
+    clientsToRooms.delete(ws);
+  });
+  */
 });
