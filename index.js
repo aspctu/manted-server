c411bf71e2653ca686fbfcc82beea39e91d1f237
Yunyu Lin <mail@yunyul.in>
2020-04-11 18:44:13 -0600

    Add replay frame support on ws

--- index.js
+++ index.js
@@ -3,42 +3,62 @@ const WebSocket = require("ws");
 const wss = new WebSocket.Server({ port: 8082 });
 
 /*
-rooms: Map<string, {host: WSCLient, guests: WSClient[]}>
+rooms: Map<string, {host: WSCLient, guests: WSClient[], replayFrames: any[] }>
 */
 
 const rooms = new Map();
 
 wss.on("connection", (ws) => {
-  // let connectedRoom = null;
+  let roomName = null;
+  let connectedRoom = null;
 
   ws.on("message", (msg) => {
     const { type, ...message } = JSON.parse(msg);
     if (type === "makeRoom") {
-      const { roomName } = message;
-      console.log("room receive", roomName);
+      roomName = message.roomName;
       if (!rooms.has(roomName)) {
         console.log(`Making room ${roomName}`);
-        rooms.set(roomName, { host: ws, guests: [] });
+        connectedRoom = { host: ws, guests: [] };
+        rooms.set(roomName, connectedRoom);
       }
     } else if (type === "joinRoom") {
-      const { roomName } = message;
+      roomName = message.roomName;
       if (rooms.has(roomName)) {
-        rooms.get(roomName).guests.push(ws);
-        connectedRoom = roomName;
+        connectedRoom = rooms.get(roomName);
+        connectedRoom.guests.push(ws);
+        ws.send(
+          JSON.stringify({
+            type: "REPLAY_BATCH",
+            frames: connectedRoom.replayFrames,
+          })
+        );
       }
     } else if (type === "frames") {
-      console.log(message);
+      for (const guest of connectedRoom.guests) {
+        guest.send(JSON.stringify({ type: "FRAMES", frames: message.frames }));
+      }
+
+      for (const frame of message.frames) {
+        // Replay frame start
+        if (frame[1]) {
+          connectedRoom.replayFrames = [];
+        }
+        connectedRoom.replayFrames.push(frame);
+      }
     }
   });
 
-  /*
   ws.on("close", (ws) => {
-    const room = rooms.get(connectedRoom) || [];
-    const idx = room.indexOf(idx);
-    if (idx > -1) {
-      room.splice(idx, 1);
+    if (connectedRoom.host === ws) {
+      rooms.delete(roomName);
+      for (const guest of connectedRoom.guests) {
+        guest.close();
+      }
+    } else {
+      const idx = connectedRoom.guests.indexOf(ws);
+      if (idx > -1) {
+        connectedRoom.guests.splice(idx, 1);
+      }
     }
-    clientsToRooms.delete(ws);
   });
-  */
 });
